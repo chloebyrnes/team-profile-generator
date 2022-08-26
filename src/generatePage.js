cc69f61646db83a4d0c7f9ae655728dd02605f4e
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-08-26 13:18:59 -0400

    added some style and linked github and email

--- src/generatePage.js
+++ src/generatePage.js
@@ -1,4 +1,4 @@
-
+// creates HTML
 
 const htmlelements = function(team) {
     return `<!DOCTYPE html>
@@ -7,10 +7,17 @@ const htmlelements = function(team) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../src/style.css" rel="stylesheet">
+    
     <title>Team Profile</title>
 </head>
 <body>
+
+<section>
+<h1>Employee Team</h1>
     ${team}
+</section>
+
     
 </body>
 </html>
@@ -18,18 +25,58 @@ const htmlelements = function(team) {
 };
 
 
-    function appendManager(manager){
-        return `
-        <div>
-        ${manager.name}
-        ${manager.id}
-        ${manager.email}
-        ${manager.number}
-        </div>`
-    }
 
 
-    function generatePage(teamMembers){
+// sections
+
+function appendManager(manager){
+    return `
+    <div class="card">
+    <div class="container">
+    <h2>Manager</h2>
+    <h4><b>Name: ${manager.name}</b></h4></p>
+    <h4><b>Employee ID: ${manager.id}</b></h4>
+    <h4><b>Email: <a href="mailto:${manager.email}">${manager.email}</a></b></h4>
+    <h4><b>Office Number: ${manager.number}</b></h4>
+    </div>
+    </div>
+    `
+}
+
+function appendEngineer(engineer){
+    return `
+    <div class="card">
+    <div class="container">
+    <h2>Engineer</h2>
+    <h4><b>Name: ${engineer.name}</b></h4>
+    <h4><b>Employee ID: ${engineer.id}</b></h4>
+    <h4><b>Email: <a href="mailto:${engineer.email}">${engineer.email}}</a></b></h4>
+    <h4><b>GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></b></h4>
+    </div>
+    </div>
+    `
+}
+
+function appendIntern(intern){
+    return `
+    <div class="card">
+    <div class="container">
+    <h2>Intern</h2>
+    <h4><b>Name: ${intern.name}</b></h4>
+    <h4><b>Employee ID: ${intern.id}</b></h4>
+    <h4><b>Email: <a href="mailto:${intern.email}">${intern.email}</a></b></h4>
+    <h4><b>School: ${intern.school}</b></h4>
+    </div>
+    </div>
+    `
+}
+
+
+
+// JS 
+
+
+function generatePage(teamMembers){
 
     
     const teamMemberCard = [];
